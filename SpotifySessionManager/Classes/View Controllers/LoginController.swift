7123df59684bbbf9785e4da3fb7b88e2fa47e32a
Julian Grosshauser <julian@juliangrosshauser.com>
2015-03-11 22:14:36 +0100

    Rename login controller
    Rename "SpotifyLoginController" to "LoginController".

--- SpotifySessionManager/Classes/View Controllers/LoginController.swift
+++ SpotifySessionManager/Classes/View Controllers/LoginController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SpotifyLoginController: UIViewController, SpotifySessionManagerDelegate {
+class LoginController: UIViewController, SpotifySessionManagerDelegate {
 
     //MARK: Initialization
 
